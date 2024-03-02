1d4b5a670686fd217094ec5a52bdb8d25957ae9c
أحمد محمود <amhmoud@moj.gov.sa>
2024-03-02 16:16:40 +0300

    Created Model classes for app entities.

---
+++ HackathonRegistration.Models/Models/TeamChallenge.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackathonRegistration.Models.Models
+{
+    public class TeamChallenge
+    {
+        public int TeamID { get; set; }
+        public Team Team { get; set; }
+
+        public int ChallengeID { get; set; }
+        public Challenge Challenge { get; set; }
+    }
+}
